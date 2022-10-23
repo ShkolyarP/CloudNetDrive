d556f4aac0e61673046aef0159b236dc6e3f2239
Vladimir Palt <paltvlad@gmail.com>
2022-10-23 15:32:14 +0300

    Initial commit

---
+++ cloudServer/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.paltvad.cloudNetDrive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
